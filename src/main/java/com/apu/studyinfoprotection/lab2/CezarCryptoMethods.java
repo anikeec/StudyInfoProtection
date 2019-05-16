42efd6871b701c170b2ed4a2bad1e55bfaa0c252
apu <pasha_anik@ukr.net>
2019-05-16 15:14:24 +0300

    Increase alphabet

--- src/main/java/com/apu/studyinfoprotection/lab2/CezarCryptoMethods.java
+++ src/main/java/com/apu/studyinfoprotection/lab2/CezarCryptoMethods.java
@@ -14,13 +14,19 @@ public class CezarCryptoMethods {
 //    protected static final int nEng = 26;  //Мощность алфавита
 //    protected static final int nRus = 33;
 
-    protected static char[] engslish = { 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+    protected static char[] engslish = {'A', 'B', 'C', 'D', 'E', 'F', 'G',
                     'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-                    'U', 'V', 'W', 'X', 'Y', 'Z' };
+                    'U', 'V', 'W', 'X', 'Y', 'Z',
+                                        'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                    'u', 'v', 'w', 'x', 'y', 'z'};
 
     protected static char[] russian = { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж',
                     'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У',
-                    'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я' };
+                    'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
+                                        'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж',
+                    'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
+                    'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
     
     protected static int NUMBER_OF_ENG = engslish.length;
     protected static final int NUMBER_OF_RUS = russian.length;
